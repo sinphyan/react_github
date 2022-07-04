2b6660dacb526a0039f67a35a7d8406712d9bc56
sinphyan <sin.wu@alleypin.com>
2022-07-05 00:43:42 +0800

    v3-0304

---
+++ V3_03-04/Item.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Item extends Component{
+    render(){
+        return(
+            <li>這是測試 第一次v3.3~3.4組件區域</li>
+        )
+    }
+}
+
+export default Item;
